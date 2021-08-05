96ba206152ffb91d379cb00e2e068da6a4ba74f7
Shivam Kumar <shivam.saha06@gmail.com>
2021-08-05 14:06:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
-var bodyParser=require("body-parser");
+var bodyParser = require("body-parser");
 var express = require("express");
-var mongoose=require("mongoose");
-
-const route=require('./routes/route');
-
-var cors=require("cors");
-var path=require("path");
-
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use('/api',route)
-
-
-
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
-
-app.use(cors());
-
-
-app.use(express.static(path.join(__dirname,'public')))
-
-
-var app=express()
-const port=3000;
-app.get('/',(req,res)=>{
-    res.send("foobar");
-});z
-app.listen(port,()=>{
-    console.log("server started at:"+port);
-})
-
-
+var mongoose = require("mongoose");
+var app = express();
 //connect to mongodb
 
 mongoose.connect("mongodb://localhost:27017/contactlist");
 
-mongoose.connection.on('connected',()=>{
+mongoose.connection.on("connected", () => {
   console.log("connected to db");
-})
+});
 
-
-
-mongoose.connection.on('error',()=>{
-  if (err){
+mongoose.connection.on("error", () => {
+  if (err) {
     console.log(err);
   }
+});
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+// const route = require("./routes/route");
+
+var cors = require("cors");
+var path = require("path");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use("/api", route);
+
+app.use(cors());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("foobar");
+});
+
+app.listen(port, () => {
+  console.log("server started at:" + port);
 });
